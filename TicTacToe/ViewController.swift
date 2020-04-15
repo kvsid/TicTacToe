0e9a8ccefa2c5e8d6123bfa9220b9adb74cf629b
kvsid <nice.curly2013@gmail.com>
2020-04-15 18:14:22 +0300

    Initial Commit

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Xenia Sidorova on 15.04.2020.
+//  Copyright © 2020 Xenia Sidorova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
